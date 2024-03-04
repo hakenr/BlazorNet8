5e64df3856861449d111f999d3670a9f3df43df9
Robert Haken <haken@havit.cz>
2024-03-04 10:49:23 +0100

    Add project files.

---
+++ BlazorNet8/BlazorNet8.Client/Layout/MainLayout.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorNet8.Client.Layout
+{
+	public partial class MainLayout
+	{
+		private ErrorBoundary _errorBoundaryComponent;
+	}
+}
---
+++ BlazorNet8/BlazorNet8.Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using BlazorNet8.Client.DemoData;
+using Havit.Blazor.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddHxServices();
+builder.Services.AddHxMessenger();
+builder.Services.AddHxMessageBoxHost();
+
+builder.Services.AddSingleton<IDemoDataService, DemoDataService>();
+
+await builder.Build().RunAsync();
